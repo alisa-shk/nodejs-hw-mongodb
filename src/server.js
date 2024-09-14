1ded7096d295150a06adac4671ad3f9f957c037a
alisa-shk <demaliska@gmail.com>
2024-09-14 13:55:59 +0300

    first

---
+++ src/server.js
@@ -0,0 +1,79 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { env } from './utils/env.js';
+import { getAllStudents, getStudentById } from './services/students.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const PORT = 3000;
+// const PORT = Number(process.env.PORT);
+
+export const startServer = () => {
+    const app = express();
+
+    app.use(express.json());
+    app.use(cors());
+
+    app.use(
+        pino({
+            transport: {
+            target: 'pino-pretty',
+            },
+            }),
+    );
+
+    app.get('/', (req, res) => {
+        res.json({
+            message: 'Hello world!',
+            });
+    });
+
+    app.use((err, req, res, next) => {
+        res.status(500).json({
+            message: 'Something went wrong',
+            error: err.message,
+            });
+    });
+
+    // app.get('/students', async (req, res) => {
+    // });
+    app.get('/students', async (req, res) => {
+        const students = await getAllStudents();
+        res.status(200).json({
+            data: students,
+        });
+    });
+
+    // app.get('/students/:studentId', async (req, res) => {
+    // });
+    app.get('/students/:studentId', async (req, res, next) => {
+        const { studentId } = req.params;
+        const student = await getStudentById(studentId);
+        // Відповідь, якщо контакт не знайдено
+        if (!student) {
+            res.status(404).json({
+                message: 'Student not found'
+            });
+            return;
+        }
+        // Відповідь, якщо контакт знайдено
+        res.status(200).json({
+            data: student,
+        });
+    });
+
+    app.use('*', (req, res, next) => {
+        res.status(404).json({
+            message: 'Not found',
+            });
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+};
